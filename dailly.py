b876e3dd3cb893a899db70c3fbc20bbe906a9709
sandeepvvs007 <68640841+sandeepvvs007@users.noreply.github.com>
2020-07-22 15:20:50 +0530

    Add files via upload

---
+++ dailly.py
@@ -0,0 +1,25 @@
+
+for i in range(1500,2701):
+    if i % 5 ==0 and i % 7 ==0 :
+        print(i)
+
+one=int(input("please enter first number"))
+two=int(input("please enter second number"))
+oper=input("please enter A for addition S for substraction M for multiplication D for division and X for exit").casefold()
+while oper is not "e" :
+    if oper =="a":
+        print("sum is {}".format(one+two))
+        break
+    elif oper == "s":
+        print("difference is {}".format(one-two))
+        break
+    elif oper == "m":
+        print("product is {}".format(one*two))
+        break
+    elif oper == "d":
+        print("division result is {}".format(one/two))
+        break
+    else:
+        print("please enter a valid input")
+        break
+
