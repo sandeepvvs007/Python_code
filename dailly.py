35b915635ffc816f5d69c5adcf6c62c36e01b829
sandeepvvs007 <68640841+sandeepvvs007@users.noreply.github.com>
2020-07-22 17:12:29 +0530

    Update dailly.py

--- dailly.py
+++ dailly.py
@@ -1,20 +1,17 @@
 one=int(input("please enter first number"))
 two=int(input("please enter second number"))
 oper=input("please enter A for addition S for substraction M for multiplication D for division and X for exit").casefold()
-while oper is not "e" :
+while oper != "e" :
     if oper =="a":
         print("sum is {}".format(one+two))
-        break
     elif oper == "s":
         print("difference is {}".format(one-two))
-        break
     elif oper == "m":
         print("product is {}".format(one*two))
-        break
     elif oper == "d":
         print("division result is {}".format(one/two))
-        break
     else:
         print("please enter a valid input")
-        break
-
+    one=int(input("please enter first number"))
+    two=int(input("please enter second number"))
+    oper=input("please enter A for addition S for substraction M for multiplication D for division and X for exit").casefold()
